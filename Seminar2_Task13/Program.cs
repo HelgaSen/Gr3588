12c91752d4f9694abce1c8892bf0ed4aa340d5fa
Helga <helgasencukova@gmail.com>
2022-12-04 22:26:50 +0300

    solutions for tasks 10,13,15 added

---
+++ Seminar2_Task13/Program.cs
@@ -0,0 +1,22 @@
+﻿//Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+string digit = Console.ReadLine() ?? "0";
+int checkNumber = 0;
+if(int.TryParse(digit,out checkNumber))
+{
+    if(digit.Length >=3) 
+        {
+            char[] array = new char[digit.Length]; 
+            array = digit.ToCharArray(0,digit.Length);
+            Console.WriteLine(array[2]);    
+        }
+    else
+        {
+            Console.WriteLine("Третьей цифры нет");   
+        }
+}
+else
+{
+    Console.WriteLine("Вы ввели не число");   
+}
---
+++ Seminar2_Task15/Program.cs
@@ -0,0 +1,32 @@
+﻿//Программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void DayOfWeekCheck(int day)
+{
+    string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(day));
+    if (day >= 1 && day <= 5)
+    {
+        Console.WriteLine("Сегодня " + outDayOfWeek + " - это рабочий день");
+    }
+    if (day == 0 || day == 6)
+    {
+        Console.WriteLine("Сегодня " + outDayOfWeek + " - это выходной день");
+    }
+}
+
+    Console.WriteLine("Введите число от 0 до 6");
+    int weekDay = int.Parse(Console.ReadLine()??"0");
+    if(weekDay >=0 && weekDay <=7)
+    {
+        DayOfWeekCheck(weekDay);
+    }
+    else
+    {
+        Console.WriteLine("Данные введены неверно. попробуйте еще раз");
+    }
+
+
+
+
+
+
+
