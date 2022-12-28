a82b698befa4e7dc0e05a438379ce78c53fa9c2b
Helga <helgasencukova@gmail.com>
2022-12-28 23:03:48 +0300

    solutions for tasks 60,62 added

---
+++ Seminar8_Task60/Program.cs
@@ -0,0 +1,118 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+int x = ReadData("Введите количество элементов X ");
+int y = ReadData("Введите количество элементов Y ");
+int z = ReadData("Введите количество элементов Z ");
+
+// int[,,] arr3D = Fill3DArray(x, y, z, 0, 9);
+// Print3DArrayColor(arr3D);
+// Console.WriteLine();
+
+int listInfo = ReadData("Какими неповторяющимися числами будем заполнять массив? Введите ответ в формате цифры, где 2 - двузначные, 3 - трехзначные и т.д. Ваш ответ: ");
+
+int listLen = (int)Math.Pow(10,listInfo)-(int)Math.Pow(10,listInfo-1);
+int statrPos = (int)Math.Pow(10,listInfo-1);
+
+if(x*y*z < listLen)
+{
+List<int> numbers = FillList(listLen,statrPos);
+int[,,] array3D = Fill3DArrayUnicNums(x,y,z,numbers);
+Print3DArrayColor(array3D);
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Вывести массив с указанными параметрами невозможно, т.к. числа начнут повторяться");
+    Console.ResetColor();
+}
+
+//============МЕТОДЫ============
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Создание списка последовательных целых чисел в заданном диапазоне
+List<int> FillList(int listLength, int startNum)
+{
+    List<int> numbers = new List<int>();
+    for (int i = 0; i < listLength; i++)
+    {
+        numbers.Add(startNum + i);
+    }
+    return numbers;
+}
+
+// // Генерируем трехмерный массив, заполненный случайными числами
+// int[,,] Fill3DArray(int countRow, int countColumn, int count3, int topBorder, int downBorder)
+// {
+//     System.Random rand = new System.Random();
+//     int[,,] array3D = new int[countRow, countColumn, count3];
+//     for (int i = 0; i < countRow; i++)
+//     {
+//         for (int j = 0; j < countColumn; j++)
+//         {
+//             for (int k = 0; k < count3; k++)
+//             {
+//                 array3D[i, j, k] = rand.Next(topBorder, downBorder + 1);
+//             }
+//         }
+//     }
+//     return array3D;
+// }
+
+//Печать трехмерного массива с цветовой раскраской
+void Print3DArrayColor(int[,,] matrix)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.ForegroundColor = col[matrix[i, j, k] % 15];
+                Console.Write(matrix[i, j, k]);
+                Console.ResetColor();
+                Console.Write("(" + i + "," + j + "," + k + ")" + "     ".Substring(matrix[i, j, k].ToString().Length));// + "     ".Substring(matrix[i, j, k].ToString().Length)
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+// Выбор случайного числа из списка с последующим удалением 
+int GenUnicNum(List<int> num)
+{
+    int index = new Random().Next(0, num.Count);
+    int outnum = num[index];
+    num.RemoveAt(index);
+    return outnum;
+}
+
+//Заполнение трехмерного массива неповторяющимися числами N.B.(!) Метод в методе
+int[,,] Fill3DArrayUnicNums(int countRow, int countColumn, int count3, List<int> nums)
+{
+    int[,,] array3D = new int[countRow, countColumn, count3];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            for (int k = 0; k < count3; k++)
+            {
+                array3D[i, j, k] = GenUnicNum(nums);
+            }
+        }
+    }
+    return array3D;
+}
+
---
+++ Seminar8_Task62/Program.cs
@@ -0,0 +1,66 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+
+int squareSide = ReadData("Введите целое число ");
+int[,] array = new int[squareSide, squareSide];
+int i = 0; //задаем переменную для индекса по строке
+int j = 0; //задаем переменную для индекса по столбцу
+int step = 1; //переменная "шага" и первый элемент массива. 
+
+while (squareSide != 0) //цикл будет работать, пока есть куда "шагать"
+{
+    int k = 0; //переменная для использования в циклах, чтобы считать шаги
+    do { array[i, j++] = step++; } while (++k < squareSide - 1); //нужно, чтобы при нечетной длине стороны серединка не оставалась пустой. Инкремент ++ ставим перед переменной, т.к. сначала получаем значение увеличенной переменной, а потом работаем с резульататом
+    for (k = 0; k < squareSide - 1; k++) //тут у нас индекс по столбцу уже накоплен. Для первого прохода он равен максимальному. Записываем следующие числа в крайний правый столбец, увеличивая индекс строки, т.е. спускаемся вниз
+    {
+        array[i++, j] = step++;
+    }
+    for (k = 0; k < squareSide - 1; k++)//сейчас мы в нижнем правом углу квадрата с указанной стороной. Идем влево по нижней строке, т.е. уменьшаем индекс столбца.
+    {
+        array[i, j--] = step++;
+    }
+    for (k = 0; k < squareSide - 1; k++)//теперь мы в нижнем левом углу. Идем вверх, уменьшая индекс строки. Индекс столбца теперь снова 0.
+    {
+        array[i--, j] = step++;
+    }
+    i++; 
+    j++;//перемещаемся вниз вправо по диагонали для продолжения
+    if (squareSide < 2)
+    {
+        squareSide = 0;
+    }
+    else
+    {
+        squareSide = squareSide - 2; //рамка уже заполнена, т.е. нужно заполнить квадрат внутри. Его сторона на 2 короче заданной.
+    }
+}
+Print2DArrayColor(array);
+
+//============МЕТОДЫ==============
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+//Печать двумерного массива с цветовой раскраской
+void Print2DArrayColor(int[,] matrix)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[matrix[i, j] % 15];
+            Console.Write(matrix[i, j] + "     ".Substring(matrix[i, j].ToString().Length));
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
