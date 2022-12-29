e465822b069abeae7ca6a5abb127186b5cf692f8
Helga <helgasencukova@gmail.com>
2022-12-29 16:28:46 +0300

    solutions for tasks 63,65,67,69 added

---
+++ Seminar9_Task67/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая примает на вход число и возвращает сумму цифр числа
+
+int numberN = ReadData("Enter number N: ");
+int Sum = RecSumDig(numberN);
+PrintResult(Sum);
+
+//==========МЕТОДЫ==============
+
+//Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Вывод результата в консоль
+void PrintResult(int prefix)
+{
+    Console.Write(prefix);
+}
+
+//Рекурентный метод нахождения суммы цифр числа
+int RecSumDig(int n)
+{
+    if (n<= 10) return n;
+    int result = n%10 + RecSumDig(n/10);
+    return result;
+}
